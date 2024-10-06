8cc43c17a102e211bb54e99cd5c0a8ef96befc1f
Nayely Giles Valdez <nayelygilesvaldez@gmail.com>
2024-10-06 09:37:16 -0400

    :package: Importando dependencias CSS y JS de Bootstrap, FontAwesome y animate.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import 'bootstrap/dist/bootstrap/bootstrap.min.css'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import 'animate.css'
 import App from './App.jsx'
 import './index.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
