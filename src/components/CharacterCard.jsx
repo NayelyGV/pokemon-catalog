f4c0fb6214c60a3072e3011135711e3900664a20
Nayely Giles Valdez <nayelygilesvaldez@gmail.com>
2024-10-06 10:06:15 -0400

    :sparkles: Añadiendo nuevas carpetas y archivos

---
+++ src/components/CharacterCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CharacterCard = () => {
+    return (
+        <div>
+            <h2>Character Card</h2>
+        </div>
+    );
+}
+
+export default CharacterCard;
