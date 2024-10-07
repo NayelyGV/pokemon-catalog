9ca02225de15bbc5dd7618eff1956107d5c085bb
Nayely Giles Valdez <nayelygilesvaldez@gmail.com>
2024-10-07 15:48:11 -0400

    :art: Renombrando a index.jsx y actualizando código

---
+++ src/index.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import App from './App';
+import './App.css';
+
+const theme = createTheme({
+});
+
+const root = ReactDOM.createRoot(document.getElementById('root')); 
+root.render(
+  <ThemeProvider theme={theme}>
+      <App />
+  </ThemeProvider>
+);
