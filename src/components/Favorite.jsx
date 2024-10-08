2fd329de2d53172074a9530da30f3615e2e3481a
Nayely Giles Valdez <nayelygilesvaldez@gmail.com>
2024-10-07 20:57:54 -0400

    :sparkles: Introduciendo nueva funcionalidad en el componente Favorite

---
+++ src/components/Favorite.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Pokemon from "./Pokemon"; // Asegúrate de tener la ruta correcta
+
+const Favorite = ({ favorites, toggleFavorite }) => {
+    return (
+        <div className="favorites">
+            <h2>Favoritos</h2>
+            <div className="favorites__container">
+                {favorites.length > 0 ? (
+                    favorites.map((fav) => (
+                        <Pokemon
+                            key={fav.id}
+                            id={fav.id}
+                            image={fav.sprites.other.dream_world.front_default ? fav.sprites.other.dream_world.front_default : fav.sprites.other['official-artwork'].front_default}
+                            name={fav.name}
+                            type={fav.types}
+                            onElemClick={() => { /* Aquí puedes agregar lógica para mostrar más info si lo deseas */ }}
+                            onFavoriteClick={() => toggleFavorite(fav)} // Manejador de favoritos
+                        />
+                    ))
+                ) : (
+                    <p>No hay Pokémon favoritos.</p>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default Favorite;
