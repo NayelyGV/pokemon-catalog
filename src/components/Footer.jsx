df70fe78c7bae525ddc751799ced8e90c2ac81d2
Nayely Giles Valdez <nayelygilesvaldez@gmail.com>
2024-10-08 04:03:30 -0400

    :lipstick: Actualizando el diseño del footer con íconos adicionales, imagen y texto.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,23 +1,43 @@
 import React from 'react';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import FacebookIcon from '@mui/icons-material/Facebook';
+import InstagramIcon from '@mui/icons-material/Instagram';
 import '../styles/footer.css'
 
 class Footer extends React.Component {
-  openGithub = () => {
-        window.open("https://github.com/NayelyGV/pokemon-catalog");
-    }
+  openLink = (url) => {
+    window.open(url);
+  }
 
-    render() {
-        return (
-            <>
-                <div className="app__footer noselect">
-                    <div onClick={this.openGithub} className="github__icon">
-                        <GitHubIcon></GitHubIcon>
-                    </div>
-                </div>
-            </>
-        )
-    }
+  render() {
+    return (
+      <footer className="app__footer noselect">
+        <div className="footer__content">
+          <img 
+            src="/src/assets/images/pokeAPI.png" 
+            alt="Logo" 
+            className="footer__logo" 
+          />
+          <p className="footer__text">© 2024 Pokémon Catalog </p>
+          <div className="social__icons">
+            <div onClick={() => this.openLink("https://github.com/NayelyGV/pokemon-catalog")} className="social__icon">
+              <GitHubIcon />
+            </div>
+            <div onClick={() => this.openLink("https://www.linkedin.com/in/nayely-giles-valdez-74b034163/")} className="social__icon">
+              <LinkedInIcon />
+            </div>
+            <div onClick={() => this.openLink("https://facebook.com")} className="social__icon">
+              <FacebookIcon />
+            </div>
+            <div onClick={() => this.openLink("https://instagram.com")} className="social__icon">
+              <InstagramIcon />
+            </div>
+          </div>
+        </div>
+      </footer>
+    );
+  }
 }
 
 export default Footer;
