3f463e05151f544bc81806df4296b572b78dac5b
Nayely Giles Valdez <nayelygilesvaldez@gmail.com>
2024-10-09 00:45:55 -0400

    :lipstick: Actualizando el estilo y funcionalidad del header, agregando un navbar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,27 +3,25 @@ import GitHubIcon from '@mui/icons-material/GitHub';
 import Pokedex from "../assets/images/pokeAPI.png";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
-
+import '../styles/Header.css';
 
 class Header extends React.Component {
     constructor(props) {
         super(props);
-        // Inicializa el estado con isChecked dependiendo del tema actual
-        const initialTheme = document.documentElement.getAttribute('data-theme') || 'dark'; // Si no hay tema, por defecto es 'light'
-        document.documentElement.setAttribute('data-theme', initialTheme); // Asegurarse de que el tema inicial se establece
+        const initialTheme = document.documentElement.getAttribute('data-theme') || 'dark';
+        document.documentElement.setAttribute('data-theme', initialTheme);
         this.state = {
-            isChecked: initialTheme === 'dark' // Comprobar el tema actual al inicio
+            isChecked: initialTheme === 'dark',
+            menuOpen: false, 
+            currentPage: window.location.pathname,
         };
     }
+
     changeTheme = () => {
         const currentTheme = document.documentElement.getAttribute('data-theme');
         const targetTheme = currentTheme === "light" ? "dark" : "light";
         
-        // Actualiza el estado y el atributo del tema
-        this.setState({
-            isChecked: targetTheme === "dark" // Actualiza el estado según el nuevo tema
-        });
-
+        this.setState({ isChecked: targetTheme === "dark" });
         document.documentElement.setAttribute('data-theme', targetTheme);
     }
 
@@ -31,31 +29,88 @@ class Header extends React.Component {
         window.open("https://github.com/NayelyGV/pokemon-catalog");
     }
 
+    goToHome = () => {
+        this.setState({ currentPage: '/' });
+        window.location.href = '/';
+    }
+
+    goToFavorites = () => {
+        this.setState({ currentPage: '/favorites' });
+        window.location.href = '/favorites'; 
+    }
+
+    toggleMenu = () => {
+        this.setState(prevState => ({ menuOpen: !prevState.menuOpen }));
+    }
+
     render() {
         return (
-            <>
-                <div className="app__header">
-                    <div className="switch">
+            <div className="app__header">
+                {/* Logo */}
+                <div className="poke__logos noselect">
+                    <img src={Pokedex} alt="pokelogo" className="poke__logo" />
+                </div>
 
-                        <div className="toggle">
-                            <label htmlFor="themeSwitch"></label>
-                            <input type="checkbox" name="swich-theme" id="themeSwitch" onClick={this.changeTheme} defaultChecked />
-                            <div className="toggle-bg"></div>
-                            <div className="toggle-thumb">
-                                <FontAwesomeIcon icon={faSun} />
-                                <FontAwesomeIcon icon={faMoon} />
+                {/* Botones para escritorio */}
+                <div className="header__buttons">
+                    <button
+                        className={`header__button ${this.state.currentPage === '/' ? 'active' : ''}`}
+                        onClick={this.goToHome}
+                    >
+                        Home
+                    </button>
+                    <button
+                        className={`header__button ${this.state.currentPage === '/favorites' ? 'active' : ''}`}
+                        onClick={this.goToFavorites}
+                    >
+                        Favorites
+                    </button>
+                </div>
+
+                {/* Icono GitHub */}
+                <div className="pokeball__box github__icon" onClick={this.openGithub}>
+                    <GitHubIcon />
+                </div>
+
+                {/* Dia/Noche */}
+                <div className="switch">
+                    <div className="toggle">
+                        <label htmlFor="themeSwitch"></label>
+                        <input type="checkbox" id="themeSwitch" onClick={this.changeTheme} defaultChecked={this.state.isChecked} />
+                        <div className="toggle-bg"></div>
+                        <div className="toggle-thumb">
+                            <FontAwesomeIcon icon={faSun} />
+                            <FontAwesomeIcon icon={faMoon} />
+                        </div>
+                    </div>
+                </div>
+
+                {/* Icono de menú para móvil */}
+                <div className="menu__toggle" onClick={this.toggleMenu}>
+                    <i className="fas fa-bars"></i>
+                </div>
+
+                {/* Menú móvil */}
+                <div className={`menu ${this.state.menuOpen ? 'active' : ''}`}>
+                    <div className="menu__item" onClick={this.goToHome}>Home</div>
+                    <div className="menu__item" onClick={this.goToFavorites}>Favorites</div>
+                    <div className="menu__item" onClick={this.openGithub}>GitHub</div>
+                    <div className="menu__item">
+                        <div className="switch">
+                            <div className="toggle">
+                                <label htmlFor="themeSwitch"></label>
+                                <input type="checkbox" id="themeSwitch" onClick={this.changeTheme} defaultChecked={this.state.isChecked} />
+                                <div className="toggle-bg"></div>
+                                <div className="toggle-thumb">
+                                    <FontAwesomeIcon icon={faSun} />
+                                    <FontAwesomeIcon icon={faMoon} />
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div className="poke__logos noselect">
-                        <img src={Pokedex} alt="pokelogo" className="poke__logo" />
-                    </div>
-                    <div className="pokeball__box github__icon" onClick={this.openGithub}>
-                        <GitHubIcon></GitHubIcon>
-                    </div>
                 </div>
-            </>
-        )
+            </div>
+        );
     }
 }
 
