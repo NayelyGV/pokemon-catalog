59c09ba259b15fd0d61a9908607200775164bf81
Nayely Giles Valdez <nayelygilesvaldez@gmail.com>
2024-10-09 11:56:24 -0400

    :art: Mejorando la estructura y estilo del header, convirtiendo a componente funcional

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,117 +1,117 @@
-import React from 'react';
+import React, { useState, useEffect }  from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import Pokedex from "../assets/images/pokeAPI.png";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
+import { faMoon, faSun, faBars } from '@fortawesome/free-solid-svg-icons';
 import '../styles/Header.css';
 
-class Header extends React.Component {
-    constructor(props) {
-        super(props);
+const Header = () => {
+    const navigate = useNavigate();
+    const location = useLocation();
+    
+    // Estado para tema y menú móvil
+    const [isChecked, setIsChecked] = useState(document.documentElement.getAttribute('data-theme') === 'dark');
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    // Efecto para aplicar el tema al montar el componente
+    useEffect(() => {
         const initialTheme = document.documentElement.getAttribute('data-theme') || 'dark';
         document.documentElement.setAttribute('data-theme', initialTheme);
-        this.state = {
-            isChecked: initialTheme === 'dark',
-            menuOpen: false, 
-            currentPage: window.location.pathname,
-        };
-    }
+    }, []);
 
-    changeTheme = () => {
-        const currentTheme = document.documentElement.getAttribute('data-theme');
-        const targetTheme = currentTheme === "light" ? "dark" : "light";
-        
-        this.setState({ isChecked: targetTheme === "dark" });
-        document.documentElement.setAttribute('data-theme', targetTheme);
-    }
+    const changeTheme = () => {
+        const newTheme = isChecked ? 'light' : 'dark'; // Cambia el valor según el estado actual
+        document.documentElement.setAttribute('data-theme', newTheme);
+        setIsChecked(!isChecked); // Cambia el estado
+    };
 
-    openGithub = () => {
-        window.open("https://github.com/NayelyGV/pokemon-catalog");
-    }
+    const openGithub = () => {
+        window.open("https://github.com/NayelyGV/pokemon-catalog", '_blank');
+    };
 
-    goToHome = () => {
-        this.setState({ currentPage: '/' });
-        window.location.href = '/';
-    }
+    const goToHome = () => {
+        if (location.pathname !== '/') {
+            navigate('/');
+        }
+    };
 
-    goToFavorites = () => {
-        this.setState({ currentPage: '/favorites' });
-        window.location.href = '/favorites'; 
-    }
+    const goToFavorites = () => {
+        if (location.pathname !== '/favorites') {
+            navigate('/favorites');
+        }
+    };
 
-    toggleMenu = () => {
-        this.setState(prevState => ({ menuOpen: !prevState.menuOpen }));
-    }
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
 
-    render() {
-        return (
-            <div className="app__header">
-                {/* Logo */}
-                <div className="poke__logos noselect">
-                    <img src={Pokedex} alt="pokelogo" className="poke__logo" />
-                </div>
+    return (
+        <div className="app__header">
+            {/* Icono de menú para móvil */}
+            <div className="menu__toggle" onClick={toggleMenu}>
+                <FontAwesomeIcon icon={faBars} />
+            </div>
+            
+            {/* Logo */}
+            <div className="poke__logos noselect">
+                <img src={Pokedex} alt="pokelogo" className="poke__logo" />
+            </div>
 
-                {/* Botones para escritorio */}
-                <div className="header__buttons">
-                    <button
-                        className={`header__button ${this.state.currentPage === '/' ? 'active' : ''}`}
-                        onClick={this.goToHome}
-                    >
-                        Home
-                    </button>
-                    <button
-                        className={`header__button ${this.state.currentPage === '/favorites' ? 'active' : ''}`}
-                        onClick={this.goToFavorites}
-                    >
-                        Favorites
-                    </button>
-                </div>
+            {/* Botones para escritorio */}
+            <div className="header__buttons">
+                <button
+                    className={`header__button ${location.pathname === '/' ? 'active' : ''}`}
+                    onClick={goToHome}
+                >
+                    Home
+                </button>
+                <button
+                    className={`header__button ${location.pathname === '/favorites' ? 'active' : ''}`}
+                    onClick={goToFavorites}
+                >
+                    Favorites
+                </button>
+            </div>
 
-                {/* Icono GitHub */}
-                <div className="pokeball__box github__icon" onClick={this.openGithub}>
-                    <GitHubIcon />
-                </div>
+            {/* Icono GitHub */}
+            <div className="pokeball__box github__icon" onClick={openGithub}>
+                <GitHubIcon />
+            </div>
 
-                {/* Dia/Noche */}
-                <div className="switch">
-                    <div className="toggle">
-                        <label htmlFor="themeSwitch"></label>
-                        <input type="checkbox" id="themeSwitch" onClick={this.changeTheme} defaultChecked={this.state.isChecked} />
-                        <div className="toggle-bg"></div>
-                        <div className="toggle-thumb">
-                            <FontAwesomeIcon icon={faSun} />
-                            <FontAwesomeIcon icon={faMoon} />
-                        </div>
+            {/* Dia/Noche */}
+            <div className="switch">
+                <div className="toggle">
+                    <label htmlFor="themeSwitch"></label>
+                    <input type="checkbox" id="themeSwitch" onClick={changeTheme} defaultChecked={isChecked} />
+                    <div className="toggle-bg"></div>
+                    <div className="toggle-thumb">
+                        <FontAwesomeIcon icon={faSun} />
+                        <FontAwesomeIcon icon={faMoon} />
                     </div>
                 </div>
-
-                {/* Icono de menú para móvil */}
-                <div className="menu__toggle" onClick={this.toggleMenu}>
-                    <i className="fas fa-bars"></i>
-                </div>
-
-                {/* Menú móvil */}
-                <div className={`menu ${this.state.menuOpen ? 'active' : ''}`}>
-                    <div className="menu__item" onClick={this.goToHome}>Home</div>
-                    <div className="menu__item" onClick={this.goToFavorites}>Favorites</div>
-                    <div className="menu__item" onClick={this.openGithub}>GitHub</div>
-                    <div className="menu__item">
-                        <div className="switch">
-                            <div className="toggle">
-                                <label htmlFor="themeSwitch"></label>
-                                <input type="checkbox" id="themeSwitch" onClick={this.changeTheme} defaultChecked={this.state.isChecked} />
-                                <div className="toggle-bg"></div>
-                                <div className="toggle-thumb">
-                                    <FontAwesomeIcon icon={faSun} />
-                                    <FontAwesomeIcon icon={faMoon} />
-                                </div>
+            </div>
+                
+            {/* Menú móvil */}
+            <div className={`menu ${menuOpen ? 'active' : ''}`}>
+                <div className="menu__item" onClick={goToHome}>Home</div>
+                <div className="menu__item" onClick={goToFavorites}>Favorites</div>
+                <div className="menu__item">
+                    <div className="switch">
+                        <div className="toggle">
+                            <label htmlFor="themeSwitch"></label>
+                            <input type="checkbox" id="themeSwitch" onClick={changeTheme} defaultChecked={isChecked} />
+                            <div className="toggle-bg"></div>
+                            <div className="toggle-thumb">
+                                <FontAwesomeIcon icon={faSun} />
+                                <FontAwesomeIcon icon={faMoon} />
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
 export default Header;
